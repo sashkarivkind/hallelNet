0bb4942c862527b445771f300080deb3100ff2b9
sashkarivkind <32360798+sashkarivkind@users.noreply.github.com>
2018-05-27 12:44:14 +0300

    introduced mft simulator
    still need to bring it to be function rather than a script

--- mft/c_plots_top.m
+++ mft/c_plots_top.m
@@ -1,12 +1,13 @@
 f1=figure;
 f2=figure;
-c0=[0:0.25:1];
-N_vec=[1000,2000];
+c0=[-1:0.01:1];
+N_vec=[1000]%,2000];
 u_vec=[1];%,1];
 log10_k=[0];%:0.5:2];
 % k_vec=10.^log10_k;
-k_vec=[2,10,1000];%[2,3,4,1000];
-c12_fun_vec={@c12_u_sparse,@c12_u_sparse_ana}; %@c12_u};
+k_vec=[2,3,4,5,6,7];%[2,3,4,1000];
+c12_rec={};
+c12_fun_vec={@c12_u_sparse};%,@c12_u_sparse_ana}; %@c12_u};
 mk_vec='xod';
 clr_vec='rgbm';
 leg={};
@@ -18,7 +19,7 @@ for ii=1:length(k_vec)
         for ff=1:length(c12_fun_vec)
             N=N_vec(nn);
             k=k_vec(ii);
-            u=u_vec(jj);
+            u=u_vec(jj)/sqrt(k);
             mk=mk_vec(ff);
             clr=clr_vec(nn);
             c12_fun=c12_fun_vec{ff};
@@ -26,6 +27,7 @@ for ii=1:length(k_vec)
             %  plot(c0,c12_u(c0,u,@sign,1e5),'x-');
             
             c12=c12_fun(c0,u,@sign,1e5,N,k);
+       c12_rec{ii}=c12;
             figure(f1);
             h(leg_i)=plot(c0,c12,[mk,clr]);
             hold on;
