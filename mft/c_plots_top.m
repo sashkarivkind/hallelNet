c817ec666ff0fb6fbfb58216542adb3086f81b5e
sashkarivkind <32360798+sashkarivkind@users.noreply.github.com>
2018-06-07 18:33:10 +0300

    Reorganized directories

--- mft/c_plots_top.m
+++ mft/c_plots_top.m
@@ -14,31 +14,31 @@ leg={};
 h=[];
 leg_i=0;
 for nn=1:length(N_vec)
-for ii=1:length(k_vec)
-    for jj=1:length(u_vec)
-        for ff=1:length(c12_fun_vec)
-            N=N_vec(nn);
-            k=k_vec(ii);
-            u=u_vec(jj)/sqrt(k);
-            mk=mk_vec(ff);
-            clr=clr_vec(nn);
-            c12_fun=c12_fun_vec{ff};
-            leg_i=leg_i+1;
-            %  plot(c0,c12_u(c0,u,@sign,1e5),'x-');
-            
-            c12=c12_fun(c0,u,@sign,1e5,N,k);
-       c12_rec{ii}=c12;
-            figure(f1);
-            h(leg_i)=plot(c0,c12,[mk,clr]);
-            hold on;
-            figure(f2);
-            plot(c0,c12-c0,[mk,clr]);
-            hold on;
-            leg{leg_i}=sprintf('k=%.1f u=%.1f N=%d',k,u,N);
-            disp(k);
+    for ii=1:length(k_vec)
+        for jj=1:length(u_vec)
+            for ff=1:length(c12_fun_vec)
+                N=N_vec(nn);
+                k=k_vec(ii);
+                u=u_vec(jj)/sqrt(k);
+                mk=mk_vec(ff);
+                clr=clr_vec(nn);
+                c12_fun=c12_fun_vec{ff};
+                leg_i=leg_i+1;
+                %  plot(c0,c12_u(c0,u,@sign,1e5),'x-');
+                
+                c12=c12_fun(c0,u,@sign,1e5,N,k);
+                c12_rec{ii}=c12;
+                figure(f1);
+                h(leg_i)=plot(c0,c12,[mk,clr]);
+                hold on;
+                figure(f2);
+                plot(c0,c12-c0,[mk,clr]);
+                hold on;
+                leg{leg_i}=sprintf('k=%.1f u=%.1f N=%d',k,u,N);
+                disp(k);
+            end
         end
     end
-end
 end
 xlabel('c_{t}');
 ylabel('c_{t+1}-c_{t}');
