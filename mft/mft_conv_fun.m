0f590a51cd56739751c01442c94447bd3a5a9ccf
sashkarivkind <32360798+sashkarivkind@users.noreply.github.com>
2018-06-10 14:45:01 +0300

    added option to enforce fixed point as 1st trajectory

--- mft/mft_conv_fun.m
+++ mft/mft_conv_fun.m
@@ -9,6 +9,8 @@ nom.dinit=0.1;
 nom.ana=1;
 nom.sim_pupd=0.1;
 nom.u=1;
+nom.save_hist=0;
+nom.enforce_fp=0;
 
 nomfields=fields(nom);
 for ff=1:length(nomfields)
@@ -30,16 +32,25 @@ dd0=0.5*(1-c0);
 conv=zeros(ntries,1);
 fp=zeros(ntries,2);
 d_min_vec=2*ones(ntries,1);
+if opt.save_hist
+    d_hist_matrix=zeros(ntries,t_max);
+end
 parfor this_try=1:ntries
     d=dinit;
     conv(this_try)=0;
     fp(this_try,:)=[0,0];
-    d_hist=2*ones(t_max,1);
+    d_hist=2*ones(1,t_max);
     if ~opt.ana
         W=sparse(binoW(N,k));
         s12=sign(randn(N,1)*[1,1]);
         s12(1:round(N*d),1) = -s12(1:round(N*d),1);
         uu=u*randn(N,1)*[1,1]/sqrt(k);
+        if opt.enforce_fp
+            smat=sign(W*s12(:,1)+uu(:,1));
+            smat=smat.*s12(:,1);
+            uu=diag(smat)*uu;
+            W=sparse(diag(smat)*W);
+        end
     end
     for t=1:t_max
         if opt.ana
@@ -59,7 +70,13 @@ parfor this_try=1:ntries
             break
         end
     end
+    if opt.save_hist
+        d_hist_matrix(this_try,:)=d_hist;
+    end
 end
 o.conv=conv;
 o.d_min=d_min_vec;
 o.fp=fp;
+if opt.save_hist
+    o.d_hist_matrix=d_hist_matrix;
+end
